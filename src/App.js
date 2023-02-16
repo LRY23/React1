00715c8eb39af2f31208269de296bb0d62a0c9a6
LRY23 <yanlry.23@icloud.com>
2023-02-16 17:00:19 +0100

    Debut du projet react , feuille simplifier

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Wesh la famille</h1>
+      <h2>Bien ou quoi ?</h2> 
+    </div>
+  );
+}
+
+export default App;
